6fe1f55ec9287bc89650d56f57d73a488db09278
jvanhyning <83515204+jvanhyning@users.noreply.github.com>
2023-12-01 16:08:16 -0800

    Imu control

--- Arduino/IMU_Control/IMU_Control.ino
+++ Arduino/IMU_Control/IMU_Control.ino
@@ -1,5 +1,12 @@
 #include <Arduino_LSM9DS1.h>
-float x,y,z;
+float mx,my,mz;
+float ax,ay,az;
+
+float mx_cal,my_cal,mz_cal;
+float ax_cal,ay_cal,az_cal;
+float ALPHA,BETA,GAMMA;
+
+
 String str;
 
 void setup() {
@@ -8,7 +15,21 @@ void setup() {
 }
 
 void loop() {
-  IMU.readMagneticField(x, y, z); // Read IMU DATA
-  str = String(x) + ","+String(y)+String(z);
-  Serial.println(str);
+  IMU.readMagneticField(mx, my, mz); // Read IMU DATA
+  IMU.readAcceleration(ax, ay, az); // Read IMU DATA
+
+  mx_cal = mx - 0;
+  my_cal = my - 0;
+  mz_cal = mz - 0;
+
+  ax_cal = ax - 0;
+  ay_cal = ay - 0;
+  az_cal = az - 0;
+
+  ALPHA = atan2(ax,ay)
+  BETA = atan2(ax,az)
+  GAMMA = atan2()
+
+
+ 
 }
