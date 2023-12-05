f5214f8c181926fbac2c71c3d875d29f322c7e15
jvanhyning <83515204+jvanhyning@users.noreply.github.com>
2023-12-05 11:23:27 -0800

    Update IMU_Control.ino

--- Arduino/IMU_Control/IMU_Control.ino
+++ Arduino/IMU_Control/IMU_Control.ino
@@ -1,35 +1,65 @@
 #include <Arduino_LSM9DS1.h>
 float mx,my,mz;
 float ax,ay,az;
+float x,y,z;
+float cal_mx,cal_my,cal_mz;
+float u,v,w;
 
-float mx_cal,my_cal,mz_cal;
-float ax_cal,ay_cal,az_cal;
-float ALPHA,BETA,GAMMA;
-
-
+float alpha,beta;
 String str;
+String str2;
+String str3;
+
 
 void setup() {
   Serial.begin(9600); // Start Serial Communitcation
   IMU.begin(); // Start IMU 
+  x = 0;
+  y = 0;
+  z = 0;
 }
 
 void loop() {
-  IMU.readMagneticField(mx, my, mz); // Read IMU DATA
-  IMU.readAcceleration(ax, ay, az); // Read IMU DATA
+  //IMU.readMagneticField(mx, my, mz); // Read IMU DATA
 
-  mx_cal = mx - 0;
-  my_cal = my - 0;
-  mz_cal = mz - 0;
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax, ay, az);
+  }
 
-  ax_cal = ax - 0;
-  ay_cal = ay - 0;
-  az_cal = az - 0;
+  
+  // cal_mx = mx - (25.63);
+  // cal_my = my - (8.78);
+  // cal_mz = mz - (-28.82);
 
-  ALPHA = atan2(ax,ay)
-  BETA = atan2(ax,az)
-  GAMMA = atan2()
+  e1Angle(ax,ay,az);
+  //str = String(cal_mx)+","+String(cal_my)+","+String(cal_mz);
+  // str2 = String(ax)+","+String(ay)+","+String(az);
+  // Serial.println(str2);
 
-
+  str3 = String(u)+","+String(v)+","+String(w);
+  Serial.println(str3);
  
 }
+
+
+void e1Angle(float x,float y,float z){
+alpha = atan2(y,x);
+u = x*cos(-alpha)-y*sin(-alpha);
+v = x*sin(-alpha)+y*cos(-alpha);
+w = z;
+beta = atan2(w,u);
+u = u*cos(-beta)-w*sin(-beta);
+v = v;
+w = u*sin(-beta)+w*cos(-beta);
+}
+
+// float rotate(float x, float y,float z,float alpha,float beta){
+// int u1 = x*cos(-alpha)-y*sin(-alpha);
+// int v1 = x*sin(-alpha)+y*cos(-alpha);
+// int w1 = z;
+// u = u1*cos(-beta)-w1*sin(-beta);
+// v = v1;
+// w = u1*sin(-beta)+w1*cos(-beta);
+// }
+
+
